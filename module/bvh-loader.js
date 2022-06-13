905733f0a5bb0e900114ae4e2e9df60f95ff5368
zlatnaspirala <zlatnaspirala@gmail.com>
2022-06-13 18:26:48 +0200

    fix

--- module/bvh-loader.js
+++ module/bvh-loader.js
@@ -297,7 +297,7 @@ export class MEBvh {
           newLog2.innerHTML += '<h2>Motion</h2>';
           newLog2.innerHTML += '<p class="paragraf fixHeight" >' + motion + '</p>';
 
-          if (typeof byId('log') != 'undefined') {
+          if (byId && byId('log') !== null) {
             byId('log').appendChild(newLog2);
             byId('log').appendChild(newLog);
           }
